ffd63c377b88ca8165c6a7265956784ee2829adf
Hunterb9101 <hunterb9101@gmail.com>
2016-09-29 18:11:15 -0600

    Revert "World Builder In Progress"
    This reverts commit dc680657d338471f1e4cedec7250e0becb6a04f8.

--- src/region/RegionWindow.java
+++ src/region/RegionWindow.java
@@ -20,7 +20,7 @@ public class RegionWindow extends ConstructorClass{
 	
 	public void init(int width, int height){
 		Registry.registerMaps();
-		RegionLoader.setMap("maps/snowyMap");
+		RegionLoader.setMap("maps/SnowyMap");
 		this.setSize(640,640);
 	}
 	
